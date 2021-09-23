508d2a7e1ebca6c27928ce60ada59e404b7db150
Senthil <senthil.dhanase@gmail.com>
2021-09-23 16:57:26 +0530

    Completed DropDown

--- Selenium Pilot Project/src/basics/DropDownEg.java
+++ Selenium Pilot Project/src/basics/DropDownEg.java
@@ -1,6 +1,8 @@
 package basics;
 
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,13 +25,22 @@ WebDriver driver;
 	
 	@Test
 	public void ExploreDropDown() {
-		// Select a values from drop down
+		// Select a value from drop down
+		// We can select values by using Index, Value and VisibleText
 		WebElement dd1 = driver.findElement(By.id("dropdown1"));
+		
 		Select selectvalues = new Select(dd1);
 		
-		// We can select values by Index, Value and VisibleText
 		selectvalues.selectByIndex(1);
 		selectvalues.selectByValue("2");
 		selectvalues.selectByVisibleText("Loadrunner");
+		
+		// Find number of options in dropdown
+		WebElement dd2 = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/select"));
+		
+		Select findoptions = new Select(dd2);
+		
+		List<WebElement> getoptions = findoptions.getOptions();
+		System.out.println("No of Options in drop down => "+getoptions.size());
 	}
 }
